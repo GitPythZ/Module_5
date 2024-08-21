afd926d60d32ba85a0c82321e197b3ae790b688e
Anatoliy_Yavorskiy <yardbirds1957@gmail.com>
2024-08-21 12:53:25 +0300

    Пространство имен

--- module_4_2.py
+++ module_4_2.py
@@ -1,1 +1,16 @@
 #"Пространство имен"
+def test_function():
+    """Функция содержит вложенную функцию inner_function, которая возвращает значение:
+       "Я в области видимости test_function".
+        test_function вызывает inner_function.
+        Попробуйте вызвать inner_function вне функций."""
+
+    def inner_function():
+        x = "Я в области видимости test_function_1"
+        print(x)
+
+    inner_function()
+
+
+test_function()
+#inner_function() - питон не видит эту функцию, так как она находится в локальной зоне видимости
--- namespaces.py
+++ namespaces.py
@@ -1,4 +1,4 @@
-import this # Встроенная область видимости
+# import this # Встроенная область видимости
 # def square(x):
 #     a = x ** 2 # Локальная область видимости
 #     print(locals())
@@ -10,23 +10,23 @@ import this # Встроенная область видимости
 import math
 
 
-def square(x): # Объемлюющая функция для even
-    d = x ** 2
-    def even(x):
-        # nonlocal - если я хочу взять значение из объемлющей области, то пишу nonlocal d (имя переменной)
-        d = x * 2
-        if d % 2 == 0:
-            print("Chet")
-        else:
-            print("non-Chet")
-    even(x)
-    return d
-
-
-a = 5 # Глобальная область видимости
-b = square(4)
-print(a)
-print(b)
+# def square(x): # Объемлюющая функция для even
+#     d = x ** 2
+#     def even(x):
+#         # nonlocal - если я хочу взять значение из объемлющей области, то пишу nonlocal d (имя переменной)
+#         d = x * 2
+#         if d % 2 == 0:
+#             print("Chet")
+#         else:
+#             print("non-Chet")
+#     even(x)
+#     return d
+#
+#
+# a = 5 # Глобальная область видимости
+# b = square(4)
+# print(a)
+# print(b)
 
 # Локальная обл ==> Всеобъемлюющая ==> Глобальная ==> Встроенная
 # print(math.sqrt(a))
@@ -35,4 +35,111 @@ print(b)
 # Тогда я смогу обращаться без math - print(square(a).
 
 
+# def s():
+#     #local
+#     a, b, c = 1, 2, 4
+#     w = "HELLO"
+#     print(a, b, c, w) # могу обратиться к глобальной переменной.
+#
+#
+# w = "Hello" #global
+# s()
+# print(w)
+# w = "Hello" #global если я вызову пфункцию до определения данной переменной, то я не смогу вывести ее на экран, будет ошибка.
+# Так как поиск переменных идет из локальной области в глобальную, то он находит w в самой функции и выводит HELLO
+# сама переменная w в глобальном пространстве не изменилась, как бы существует две разные переменные w в глобальной и
+# локальной области. Это можно доказать методом id - вернет разные коды переменных.
 
+
+# def local():
+#     a = 11
+#     b = 22
+#     c = 33
+#     print(a, b, c, "local")
+#
+#
+# a = 100
+# b = 200
+# c = 300
+# local()
+# print(a, b, c, "global")
+#
+#
+# def local(a, b, c):
+#     a = 11
+#     print(a, b, c, "local")
+#
+#
+# a = [1, 2, 3, 4, 5, 6]
+# b = 200
+# c = 300
+# local(a, b, c)
+# print(a, b, c, "global")
+#
+#
+# def local(a, b, c):
+#     a[1] = 100
+#     print(a, b, c, "local")
+#
+#
+# a = [1, 2, 3, 4, 5, 6]
+# b = 200
+# c = 300
+# local(a, b, c)
+# print(a, b, c, "global")
+
+
+# def primer_global():
+#     global a
+#     a = 30
+#
+#
+# a = [1, 2, 3, 4, 5]
+# primer_global()
+#
+# print(a, "global")
+#
+#
+# def q():
+#     primer_global()
+
+
+# def s():
+#     #enclosing - объемлющая
+#     abs = 200
+#     def q():
+#         # local
+#         abs = "hello"
+#         print(abs, "q")
+#     q()
+#     print(abs, "s")
+#
+#
+# #global
+# abs = [1, 2, 3]
+# s()
+
+# Local - Локальная
+# Enclosing Объемлющая
+# Global Глобальная
+# Built in Встроенная
+# Python ищет переменные по принципу LEGB (Local, Enclosing, Global, Built in)
+
+# Вложенные функции
+g = "gray" # global
+def colors():
+    y = "yellow" # enclosing
+    g = "green" # enclosing
+    def print_red():
+        r = "red"
+        print(r, y, g)
+
+    def print_blue():
+        b = "blue"
+        print(b, y, g)
+
+    print_blue()
+    print_red()
+
+
+colors()
