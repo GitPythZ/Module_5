bad08edb957d6d21dc5463d29c660cf98b2b002d
Anatoliy_Yavorskiy <yardbirds1957@gmail.com>
2024-08-25 01:04:01 +0300

    namespaces

---
+++ vebinary/arrays.py
@@ -0,0 +1,5 @@
+from Module_5.sortfunc import insertion_sort, bubble_sort, selection_sort
+
+data_1 = [4, 5, 8, 2, 3, 4, 5, 67]
+data_2 = [4, 5, 8, 2, 3, 4, 5, 6, 3, 4, 7]
+data_3 = [4, 5, 8, 2, 3, 4, 5, 65, 73, 2, 67]
