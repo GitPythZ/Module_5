731872616776587c73d610684e465ee8c20acffe
Anatoliy_Yavorskiy <yardbirds1957@gmail.com>
2024-08-31 11:37:23 +0300

    исправлено

--- module_5_3.py
+++ module_5_3.py
@@ -9,44 +9,54 @@ class House:
     def __add__(self, other):
         if not isinstance(other, (int, House)):
             raise ArithmeticError("Значение этажей должно иметь тип int или объектом House")
+        self.number_of_flours = self.number_of_flours + other
+        return self
 
-        fl = other if isinstance(other, int) else other.number_of_flours
-        return House(self.number_of_flours + fl)
+    def __radd__(self, other):
+        if not isinstance(other, (int, House)):
+            raise TypeError("Правый операнд должен быть типом int или объектом House")
+        return self + other
+
+    def __iadd__(self, other):
+        if not isinstance(other, (int, House)):
+            raise TypeError("Правый операнд должен быть типом int или объектом House")
+        self.number_of_flours += other
+        return self
 
     def __eq__(self, other):
         if not isinstance(other, (int, House)):
             raise TypeError("Правый операнд должен быть типом int или объектом House")
-
         fl = other if isinstance(other, int) else other.number_of_flours
         return self.number_of_flours == fl
-    #
-    # def __le__(self, other):
-    #     if not isinstance(other, (bool, House)):
-    #         raise ArithmeticError("Содержит тип int или House")
-    #
-    #     return self.number_of_flours <= other.number_of_flours
-    #
-    # def __gt__(self, other):
-    #     if not isinstance(other, (bool, House)):
-    #         raise ArithmeticError("Содержит тип int или House")
-    #
-    #     return self.number_of_flours > other.number_of_flours
-    #
-    # def __ge__(self, other):
-    #     if not isinstance(other, (bool, House)):
-    #         raise ArithmeticError("Содержит тип int или House")
-    #
-    #     return self.number_of_flours >= other.number_of_flours
-    #
-    # def __ne__(self, other):
-    #     if not isinstance(other, (bool, House)):
-    #         raise ArithmeticError("Содержит тип int или House")
-    #
-    #     return self.number_of_flours != other.number_of_flours
+
+    def __le__(self, other):
+        if not isinstance(other, (bool, House)):
+            raise ArithmeticError("Содержит тип int или House")
+        return self.number_of_flours <= other.number_of_flours
+
+    def __gt__(self, other):
+        if not isinstance(other, (bool, House)):
+            raise ArithmeticError("Содержит тип int или House")
+        return self.number_of_flours > other.number_of_flours
+
+    def __ge__(self, other):
+        if not isinstance(other, (bool, House)):
+            raise ArithmeticError("Содержит тип int или House")
+        return self.number_of_flours >= other.number_of_flours
+
+    def __ne__(self, other):
+        if not isinstance(other, (bool, House)):
+            raise ArithmeticError("Содержит тип int или House")
+        return self.number_of_flours != other.number_of_flours
 
     def __str__(self):
         return f"Название: {self.name}, количество этажей: {self.number_of_flours}"
 
+    @classmethod # хотел сократить код, но что-то пошло не так
+    def __verify_data(cls, other):
+        if not isinstance(other, (bool, House)):
+            raise ArithmeticError("Содержит тип int или House")
+        return other if isinstance(other, int) else other.number_of_flours
 
 
 h1 = House("ЖК Эльбрус", 10)
@@ -55,12 +65,14 @@ print(h1)
 print(h2)
 print(h1 == h2)
 h1 = h1 + 10
-
-# print(h1.__add__(10))
-# print(h1)
-# print(h2.__add__(10))
-# print(h1 > h2) # __gt__
-# print(h1 >= h2) # __ge__
-# print(h1 < h2) # __lt__
-# print(h1 <= h2) # __le__
-# print(h1 != h2) # __ne__
+print(h1)
+print(h1 == h2)
+h1 += 10
+print(h1)
+h2 = 10 + h2
+print(h2)
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
